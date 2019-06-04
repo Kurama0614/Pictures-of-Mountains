3d7255cb979654b61cfb3105131169f4d33ff069
Kurama0614 <51371441+Kurama0614@users.noreply.github.com>
2019-06-04 09:36:55 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+function showPicture(picture){
+    $(".mountain-picture").attr("src",picture);    
+}
+$(".everest").click(function() {
+    showPicture("https://user-images.githubusercontent.com/14214/42292528-246b57fe-7fa1-11e8-83fb-3a0daa76d08e.png");
+});
+
+$(".k2").click(function() {
+   showPicture("src", "https://user-images.githubusercontent.com/14214/42292614-ba367638-7fa1-11e8-8e94-97bc8b566232.png");
+});
+
+$(".kangchenjunga").click(function() {
+    showPicture("src", "https://user-images.githubusercontent.com/14214/42292634-e0fedbac-7fa1-11e8-9d56-6d12dcd7ab3c.png");
+});
